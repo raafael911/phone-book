45432a8a4e3e388cc4c8182528cb4b8d5680c413
Kevin Neuenfeldt <kevin.neuenfeldt@posteo.de>
2018-05-21 10:34:45 +0200

    Added CORS to allow cross origin requests

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,9 @@ var phoneBookEntriesRouter = require('./routes/phone-book-entries');
 
 var app = express();
 
+var cors = require('cors')
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -18,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/phoneBookEntries', phoneBookEntriesRouter);
