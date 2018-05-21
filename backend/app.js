fc3a862fb89da51d6c92ab108f22b890b6055cc3
Kevin Neuenfeldt <kevin.neuenfeldt@posteo.de>
2018-05-21 14:58:20 +0200

    Added OPTIONS CORS Header

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Configure CORS
 app.use(cors());
+app.options('*', cors())
 
 app.use('/', indexRouter);
 app.use('/phoneBookEntries', phoneBookEntriesRouter);
