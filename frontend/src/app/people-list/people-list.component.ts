86dff60d64ecdb04acd4f7248484516432f0dceb
Kevin Neuenfeldt <kevin.neuenfeldt@posteo.de>
2018-05-22 21:36:57 +0200

    Added some documentation

--- frontend/src/app/people-list/people-list.component.ts
+++ frontend/src/app/people-list/people-list.component.ts
@@ -23,6 +23,16 @@ export class PeopleListComponent implements OnInit {
       .subscribe(entries => this.entries = entries);
   }
 
+  /** Filter function for the search box.
+  *
+  * Checks all phone numbers, firstName and lastName of `entry` if any of them
+  * somewhere contains `searchText`.  The check is case-insensitive. Any match
+  * will make this method return true.
+  *
+  * @param {PhoneBookEntry} entry to be checked for a match in `searchText`.
+  * @param {string} searchText to be searched in `entry`.
+  * @return {boolean} true if `entry` is a match for `searchText`, false otherwise.
+  */
   filterFunction(entry: PhoneBookEntry, searchText: string): boolean {
 
     if (!entry) {
